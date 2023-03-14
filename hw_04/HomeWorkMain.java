8b8598fddaeaa89b401eb769b777cf651f5e2648
Lider <liderolia@inbox.ru>
2023-03-14 09:09:40 +0700

    hw_04

--- hw_04/HomeWorkMain.java
+++ hw_04/HomeWorkMain.java
@@ -10,20 +10,20 @@ public class HomeWorkMain {
         // Нужно уметь получать сумарный вес всех фруктов в коробке
         // Нужно уметь пересыпать все фрукты из одной коробки в другую
 
-        //Box<String> stringBox = new Box<>(); // Не должно работать!
+        Box<String> stringBox = new Box<>(); // Не должно работать!
 
         Box<Apple> appleBox = new Box<>();
         appleBox.add(new Apple(3.0));
         appleBox.add(new Apple(2.0));
         appleBox.add(new GoldenApple(1.0));
-        //appleBox.add(new Orange(2.0)); // Не должно работать!
+        appleBox.add(new Orange(2.0)); // Не должно работать!
         System.out.println(appleBox.getWeight()); // 6.0
 
         Box<GoldenApple> goldenAppleBox = new Box<>();
         goldenAppleBox.add(new GoldenApple(2.0));
-        //goldenAppleBox.add(new Apple(2.0)); // Не должно работать
+        goldenAppleBox.add(new Apple(2.0)); // Не должно работать
 
-        //appleBox.moveTo(goldenAppleBox); // Не должно работать!
+        appleBox.moveTo(goldenAppleBox); // Не должно работать!
         goldenAppleBox.moveTo(appleBox);
         System.out.println(appleBox.getWeight()); // 0.0
         System.out.println(goldenAppleBox.getWeight()); // 8.0
